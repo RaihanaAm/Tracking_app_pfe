50bf7483dc9374df253405ced52bd588ad8b7a45
Raihana Am <raihana.amrani999@gmail.com>
2025-04-18 10:30:38 +0100

    Initial commit: Added Laravel backend and ASP.NET Core frontend

---
+++ frontend/Tracking_app_PFE_frontEnd/Tracking_app_PFE_frontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tracking_app_PFE_frontEnd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
